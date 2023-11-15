ca3d70d8386414efb2c850578f0d2da072c5925b
marco useche <marcouseche@protonmail.com>
2023-11-15 16:25:38 -0400

    👔 Adding functionality 👔

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.rand.calendar"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.rand.calendar"
--- app/src/main/java/com/rand/calendar/MainActivity.kt
+++ app/src/main/java/com/rand/calendar/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.rand.calendar
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var text: TextView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text = findViewById(R.id.showText)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            openDialog()
+        }
+    }
+
+    private fun openDialog() {
+        val dialog = DatePickerDialog(this)
+        dialog.show()
     }
 }
